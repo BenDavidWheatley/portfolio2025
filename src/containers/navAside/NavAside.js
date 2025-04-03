d4be3e4b018df310e686e17cc7d10db983fdcfa7
Ben Wheatley <info@wheatleystudios.com>
2025-04-03 22:52:23 +0200

    Animation fixes
    Fixing the max-width of the main container caused issues for animations on bigger screens. This is now all fixed.

--- src/containers/navAside/NavAside.js
+++ src/containers/navAside/NavAside.js
@@ -9,12 +9,13 @@ function NavAside() {
     const entered = useSelector(state => state.enteredReducer.value);   
     return (
         <section data-testid="navaside" className={Style.navAsideContainer} >
-            <Header />
-            <Navigation />   
-            <div className={entered? Style.imageContainer : Style.noDisplay}>
-                <img src={myImage} className={Style.myImage} alt="I'm Ben wheatley"/>    
-            </div>
-            
+            <div className={Style.navAsideDiv}>
+                <Header />
+                <Navigation />   
+                <div className={entered? Style.imageContainer : Style.noDisplay}>
+                    <img src={myImage} className={Style.myImage} alt="I'm Ben wheatley"/>    
+                </div>
+            </div>      
         </section>
     )
 }
--- src/containers/navAside/header/Header.js
+++ src/containers/navAside/header/Header.js
@@ -8,8 +8,7 @@ function Header() {
     const dispatch = useDispatch();
 
     return (
-        <section 
-            className={entered ? '': Style.headerSection} 
+        <section className={Style.headerSection}
             data-testid="header"
         >
             <div className={`${entered? Style.backgroundFade : Style.background}` }></div> 
@@ -28,41 +27,3 @@ function Header() {
 }
 
 export default Header;
-/*
-import React, { useState } from 'react';
-import Style from './header.module.css';
-import { useDispatch, useSelector } from 'react-redux';
-
-function Header() {
-    const entered = useSelector(state => state.enteredReducer.value); 
-    const dispatch = useDispatch();
-
-    
-    console.log(entered);
-    const [isEntered, setIsEntered] = useState(false);
-
-    const handleEnterClick = () => {
-        setIsEntered(true);
-    };
-
-    return (
-        <section 
-            className={isEntered ? '': Style.headerSection} 
-            data-testid="header"
-        >
-            <div className={`${isEntered? Style.backgroundFade : Style.background}` }></div> 
-                <div className={Style.headerDiv}>
-                    <h1 className={isEntered? Style.mainHeaderAnimation : Style.mainHeader}>Ben Wheatley</h1>
-                    <h2 className={isEntered? Style.subheaderAnimation : Style.subheader}>Full Stack Developer</h2>
-                </div>
-                
-                <h3 className={isEntered ? Style.mainHeaderThree : Style.noDisplay}>Oxford Based</h3>
-                <button
-                    className={`${'clickable'} ${isEntered ? Style.noDisplay : Style.enterSite}`} 
-                    onClick={handleEnterClick}
-                >Enter</button>           
-        </section>
-    );
-}
-
-export default Header;*/
